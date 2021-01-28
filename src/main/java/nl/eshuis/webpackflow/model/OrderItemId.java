7873ce744ae5981ed1149dcdd665d23edd2dded9
wilcolindeboom <wilco.lindeboom@gmail.com>
2021-01-28 21:31:35 +0100

    override Equals() and HashCode in OrderItems set spring.jpa.open-in-view=false in application.properties

--- src/main/java/nl/eshuis/webpackflow/model/OrderItemId.java
+++ src/main/java/nl/eshuis/webpackflow/model/OrderItemId.java
@@ -6,4 +6,14 @@ public class OrderItemId implements Serializable {
 
     private String sourceItemId;
     private String sourceOrderId;
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
